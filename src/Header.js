0d96933afedd13afc1877699704dc64fad3b89d0
koushikkondepati <koushikkondepati4255@gmail.com>
2020-05-23 08:55:54 +0530

    styled the Header part

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+
 const Header = function(){
+
+    const HeaderStyle= {
+        textAlign : 'center',
+        padding : 20,
+        background: '#000',
+        color: '#fff',
+        textTransform: 'uppercase'
+    };
+    
     return(
-        <div className="header">
+        <div style={HeaderStyle}>
           Phone Directory
         </div>
     )
     
 }
+
+export default Header;
